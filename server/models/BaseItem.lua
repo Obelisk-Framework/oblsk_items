aeae93ce004dc563e021ccc7e10b02a1ae3dfbe1
AndiLfl <noworkermct@gmx.ch>
2026-08-18 11:40:20 +0700

    feat(items): add BaseItem:category() belongsTo relation

--- server/models/BaseItem.lua
+++ server/models/BaseItem.lua
@@ -22,7 +22,7 @@ BaseItem.casts = {
 }
 
 function BaseItem:category()
-    return self:belongsTo(BaseItemCategory, 'base_item_category_id')
+    return self:belongsTo(ItemCategory, 'item_category_id')
 end
 
 return BaseItem
